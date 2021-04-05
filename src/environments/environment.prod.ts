2e5801dc1a649a32d28afb0494d28f0ef03002df
Adnan Khaled <adnankhaled145@gmail.com>
2021-04-05 17:21:43 +0300

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDyKFJ4m-GUhmqvHPnyXgZ9PP5iudjVrRw",
+    authDomain: "bookwarm-8015e.firebaseapp.com",
+    projectId: "bookwarm-8015e",
+    storageBucket: "bookwarm-8015e.appspot.com",
+    messagingSenderId: "675952537076",
+    appId: "1:675952537076:web:d865d6037be7e75b655da1",
+    measurementId: "G-BDYSLCDJH0"    
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDyKFJ4m-GUhmqvHPnyXgZ9PP5iudjVrRw",
+    authDomain: "bookwarm-8015e.firebaseapp.com",
+    projectId: "bookwarm-8015e",
+    storageBucket: "bookwarm-8015e.appspot.com",
+    messagingSenderId: "675952537076",
+    appId: "1:675952537076:web:d865d6037be7e75b655da1",
+    measurementId: "G-BDYSLCDJH0"   
+  }
+};
